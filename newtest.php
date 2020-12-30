e3e3b2c66ed34b63ab09900e501d094e233d3b25
ashishkpal <53655875+ashishkpal@users.noreply.github.com>
2020-12-30 15:50:04 +0530

    Update newtest.php

--- newtest.php
+++ newtest.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-this is sample file</p>'; ?> 
+ <?php echo '<p>Hello World-this is sample file with scripts</p>'; ?> 
  </body>
 </html>
